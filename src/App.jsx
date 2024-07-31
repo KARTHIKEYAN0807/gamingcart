f8f062debb54ac608b5ed711fa8edd46a66dc029
KARTHIKEYAN0807 <karthickestore.37sp@gmail.com>
2024-07-31 23:48:28 +0530

    gamecart

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     { id: 2, name: 'Call of Duty: Modern Warfare 2', description: 'First-person shooter game.', price: 49.99, image: 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/store/games/mw2/overview/CORTEZ_KA_FINISH_BLEED_BRANDED_CL_16x9_051722_01.webp' },
     { id: 3, name: 'Call of Duty: Black Ops 6', description: 'First-person shooter game.', price: 39.99, image: 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/blog/hero/bo6/BO6-REVEAL-TOUT.jpg' },
     { id: 4, name: 'Call of Duty: Black Ops Cold War', description: 'First-person shooter game.', price: 69.99, image: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1985810/header.jpg?t=1686345977' },
-    { id: 5, name: 'Call of Duty: Warzone', description: 'Battle Royale game.', price: 0.00, image: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1962663/header.jpg?t=1721859504' },
+    { id: 5, name: 'Call of Duty: Warzone', description: 'Battle Royale game,First-person shooter game.', price: 0.00, image: 'https://shared.akamai.steamstatic.com/store_item_assets/steam/apps/1962663/header.jpg?t=1721859504' },
     { id: 6, name: 'Call of Duty: Vanguard', description: 'First-person shooter game.', price: 59.99, image: 'https://www.callofduty.com/content/dam/atvi/callofduty/cod-touchui/store/games/vanguard/overview/VanguardPDP_Hero.png' },
   ]);
 
